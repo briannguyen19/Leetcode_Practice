7685e727d345e24173050fb76652af95bd21f93e
khangtnguyen19 <80604127+khangtnguyen19@users.noreply.github.com>
2023-12-26 19:03:02 -0500

    Time: 2 ms (91.92%), Space: 46.9 MB (5.52%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
@@ -4,9 +4,13 @@ class Solution {
         int max = -1;
         
         for (int i = n - 1; i >= 0; i--) {
-            int current = arr[i];
-            arr[i] = max;
-            max = Math.max(max, current);
+            if (arr[i] > max) {
+                int tmp = max;
+                    max = arr[i];
+                    arr[i] = tmp;
+            } else {
+                arr[i] = max;
+            }
         }
         return arr;
     }
