daa98f83110089a151fbdb8d2a96fccfa7601ec1
khangtnguyen19 <80604127+khangtnguyen19@users.noreply.github.com>
2024-01-15 23:17:54 -0600

    Time: 1 ms (97.91%), Space: 45.5 MB (28.18%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -18,7 +18,6 @@ class Solution {
         
         // Optimal solution:
         // If no flowers need to be planted, return true
-        // int count = 0;
         for (int i = 0; i < flowerbed.length; i++) {
             // Check if the current plot is empty.
             if (flowerbed[i] == 0) {
@@ -29,7 +28,6 @@ class Solution {
                 // If both plots are empty, we can plant a flower here.
                 if (emptyLeftPlot && emptyRightPlot) {
                     flowerbed[i] = 1;
-                    // count++;
                     n--;
                 }
             }
